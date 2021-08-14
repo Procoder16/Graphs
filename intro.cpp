b92c618bcc3e3fbf2e1e4ce2919e325607954d30
Procoder16 <smsoumik1608@gmail.com>
2021-08-14 17:38:57 +0530

    graph intro added with adjacency matrix and list

---
+++ intro.cpp
@@ -0,0 +1,41 @@
+//This file basically contains the two methods of storing the values in graph
+//First I will be writing the code for the Adjacency matrix and then Adjacency List
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    int n, m;
+    cout<<"ENTER THE NUMBER OF NODES:";
+    cin>>n;
+    cout<<"ENTER THE NUMBER OF EDGES:";
+    cin>>m;
+
+    //ADJACENCY MATRIX
+
+    // int adj[n+1][n+1] = {0};
+
+    // for(int i = 0; i < m; i++){
+    //     int u,v;
+    //     cin>>u>>v;
+
+    //     adj[u][v] = 1;
+    //     adj[v][u] = 1;
+    // }
+
+    //ADJACENCY LIST
+    
+    vector<int> adj[6];
+
+    for(int i = 0; i < m; i++){
+        int u, v;
+        cin>>u>>v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    return 0;
+}
