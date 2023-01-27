6db0d48f3409b90abb0428a23bdc8ab8d8586b43
Procoder16 <smsoumik1608@gmail.com>
2023-01-28 00:52:36 +0530

    disjoint set and prims algo added

--- disjointSet.cpp
+++ disjointSet.cpp
@@ -1,66 +1,84 @@
-//This code would be just the basic implementation of the Disjoint Set Data Structure
-
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int parent[10000];
-int rankArr[10000];
+class DisjointSet{
+    vector<int> parent, rank, size;
 
-void makeSet(int n){
-    for(int i = 0; i < n; i++){
-        parent[i] = i;
-        rankArr[i] = 0;
-    }
-}
+    public:
 
-int findPar(int node){
-    if(parent[node] == node){
-        return node;
-    }
-    //This is the code without path compression
-    // else{
-    //     findPar(parent[node]);
-    // }
+    DisjointSet(int n){
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
 
-    //With Path Compression
-    else{
-        parent[node] = findPar(parent[node]);
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+        }
     }
-}
 
-void Union(int u, int v){
-    u = findPar(u);
-    v = findPar(v);
+    int findUltParent(int n){
+        if(n == parent[n]){
+            return n;
+        }
 
-    if(rankArr[u] < rankArr[v]){
-        parent[u] = v;
+        return parent[n] = findUltParent(parent[n]);
     }
-    else if(rankArr[v] < rankArr[u]){
-        parent[v] = u;
+
+    void unionByRank(int u, int v){
+        int parU = findUltParent(u);
+        int parV = findUltParent(v);
+
+        if(parU == parV)
+            return;
+        if(rank[parU] < rank[parV])
+            parent[parU] = parV;
+        else if(rank[parV] < rank[parU])
+            parent[parV] = parU;
+        else{
+            parent[parV] = parU;
+            rank[parU]++;
+        }      
     }
-    else{
-        parent[v] = u;
-        rankArr[u]++;
+
+    void unionBySize(int u, int v){
+        int parU = findUltParent(u);
+        int parV = findUltParent(v);
+
+        if(parU == parV)
+            return;
+        if(size[parU] < size[parV])
+            parent[parU] = parV;
+        else{
+            parent[parV] = parU;
+            size[parU] += size[parV];
+        }      
     }
-}
+};
 
 int main(){
 
-    int n =10;
-    makeSet(10);
+    DisjointSet ds(7);
 
-    int m;
-    cin>>m;
-    while(m--){
-        int u, v;
-        cin>>u>>v;
-        Union(u, v);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
+
+    // if 3 and 7 belong to the same component
+    if(ds.findUltParent(3) == ds.findUltParent(7)){
+        cout<<"Same\n";
+    }else{
+        cout<<"Not same\n";
     }
 
-    //If asked to check whether two node belon to the same component
-    int a, b;
-
-    return(findPar(a) == findPar(b));
+    ds.unionBySize(3, 7);
+    // if 3 and 7 belong to the same component
+    if(ds.findUltParent(3) == ds.findUltParent(7)){
+        cout<<"Same\n";
+    }else{
+        cout<<"Not same\n";
+    }
 
     return 0;
 }
